60e3977f02d768b202dc05b6c8a2ae13a0aee2e6
Kate-AC <kate-ac@Kate-AC-no-MacBook-Pro.local>
2021-05-10 14:15:19 +0900

    fix lint

--- front/src/tests/setup.ts
+++ front/src/tests/setup.ts
@@ -1,6 +1,6 @@
+import { jest } from '@jest/globals';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 jest.useFakeTimers();
 Enzyme.configure({ adapter: new Adapter() });
-
